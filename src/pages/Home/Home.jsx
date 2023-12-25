e20d6864cd3b6a38497d246e74a3468904157a84
mdhasanali39 <contact.hasankhanyt@gmail.com>
2023-12-25 18:37:45 +0600

    Home: page updated with whoUsingBenefited component

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import Banner from "../../components/Banner/Banner";
+import WhoUsingBenefited from "../../components/TaskManagement/WhoUsingBenefited/WhoUsingBenefited";
 
 const Home = () => {
     return (
         <div>
             {/* banner  */}
             <Banner/>
+            <WhoUsingBenefited/>
         </div>
     );
 };
