12bdbf18da0162a4ecf7468d42c4c909417f4566
mdhasanali39 <contact.hasankhanyt@gmail.com>
2023-12-25 18:39:54 +0600

    PrivateRoute: is updated with added react-loader-spinner grid loading

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -1,11 +1,21 @@
 import PropTypes from  'prop-types'
 import useAuth from '../hooks/useAuth';
 import {Navigate} from 'react-router-dom';
+import { Grid } from 'react-loader-spinner';
 
 const PrivateRoute = ({children}) => {
     const {user, loading} = useAuth()
     if(loading){
-        return <div className="min-h-[86vh] flex justify-center items-center"><span className=""> loading....</span></div>
+        return <div className="min-h-[86vh] flex justify-center items-center"><span className=""> <Grid
+            visible={true}
+            height="80"
+            width="80"
+            color="#4fa94d"
+            ariaLabel="grid-loading"
+            radius="12.5"
+            wrapperStyle={{}}
+            wrapperClass="grid-wrapper"
+            /></span></div>
     }
     if(!user){
         return <Navigate to={'/login'} replace={true}/>
