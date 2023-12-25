6040123fc1c6f828b846aa446afe0bb1c866377b
mdhasanali39 <contact.hasankhanyt@gmail.com>
2023-12-25 18:50:07 +0600

    YourTask: there is three separeted task section with drag drop facility

--- src/components/TaskManagement/YourTasks/YourTasks.jsx
+++ src/components/TaskManagement/YourTasks/YourTasks.jsx
@@ -1,52 +1,76 @@
 import { useEffect, useState } from "react";
-import CreateTask from "../CreateTask/CreateTask";
-import { MdOutlineAddTask } from "react-icons/md";
 import TaskCard from "../TaskCard/TaskCard";
-import {useQuery} from '@tanstack/react-query'
+import { useQuery } from "@tanstack/react-query";
 import { axiosPublic } from "../../../api";
 import useAuth from "../../../hooks/useAuth";
+import { Grid } from "react-loader-spinner";
 
 const YourTasks = () => {
-  const [todo,setTodo] = useState([])
-  const [ongoing,setOngoing] = useState([])
-  const [completed,setCompleted] = useState([])
+  const [todo, setTodo] = useState([]);
+  const [ongoing, setOngoing] = useState([]);
+  const [completed, setCompleted] = useState([]);
 
-  const {user} = useAuth()
+  const { user } = useAuth();
 
-  const {data: tasks = [], refetch, isLoading, isFetching} = useQuery({
+  const {
+    data: tasks = [],
+    refetch,
+    isLoading,
+  } = useQuery({
     queryKey: ["tasks"],
-    queryFn: async()=>{
-      const {data} = await axiosPublic.get(`/get-tasks/${user?.email}`)
+    queryFn: async () => {
+      const { data } = await axiosPublic.get(`/get-tasks/${user?.email}`);
       return data;
-    }
+    },
   });
 
-  useEffect(()=>{
-    const todo = tasks.filter(t => t.status === "todo");
-    const ongoing = tasks.filter(t => t.status === "ongoing");
-    const completed = tasks.filter(t => t.status === "completed");
+  useEffect(() => {
+    const fTodo = tasks.filter((t) => t.status === "todo");
+    const fOngoing = tasks.filter((t) => t.status === "ongoing");
+    const fCompleted = tasks.filter((t) => t.status === "completed");
 
-    setTodo(todo)
-    setOngoing(ongoing)
-    setCompleted(completed)
-  },[tasks,setTodo,setOngoing,setCompleted])
+    if (
+      !arraysAreEqual(fTodo, todo) ||
+      !arraysAreEqual(fOngoing, ongoing) ||
+      !arraysAreEqual(fCompleted, completed)
+    ) {
+      setTodo(fTodo);
+      setOngoing(fOngoing);
+      setCompleted(fCompleted);
+    }
+  }, [tasks, todo, ongoing, completed]);
 
+  const arraysAreEqual = (arr1, arr2) => {
+    return JSON.stringify(arr1) === JSON.stringify(arr2);
+  };
   // console.log(tasks);
-  
 
   return (
-    <div className="relative grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+    <>
+      {isLoading ? (
+        <div className="min-h-[86vh] flex justify-center items-center"><span className=""> <Grid
+            visible={true}
+            height="80"
+            width="80"
+            color="#4fa94d"
+            ariaLabel="grid-loading"
+            radius="12.5"
+            wrapperStyle={{}}
+            wrapperClass="grid-wrapper"
+            /></span></div>
+      ) : (<div className="relative grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
       {/* todo lists */}
-      <TaskCard
-      sectionName="todo"
-        tasks={todo}
-        refetch={refetch}
-      />
+
+      <TaskCard sectionName="Todo" isCreateTask={true} tasks={todo} refetch={refetch} />
+
       {/* ongoing lists  */}
-      <TaskCard sectionName="ongoing" tasks={ongoing} isLoading={isLoading} refetch={refetch} />
+
+      <TaskCard sectionName="OnGoing" tasks={ongoing} refetch={refetch} />
+
       {/* completed lists  */}
-      <TaskCard sectionName="completed" tasks={completed} isLoading={isLoading} refetch={refetch}/>
-    </div>
+      <TaskCard sectionName="Completed" tasks={completed} refetch={refetch} />
+    </div>)}
+    </>
     
   );
 };
